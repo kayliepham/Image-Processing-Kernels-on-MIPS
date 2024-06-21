4cbd68f0dc3c6c699ca349cbabdb42c7d33d0022
kayliepham <161658889+kayliepham@users.noreply.github.com>
2024-06-20 21:57:44 -0700

    Update lab5.s

--- lab5.s
+++ lab5.s
@@ -1,16 +1,6 @@
 jal main
 #                         CS 240, Lab #5
-#
-#      IMPORTANT NOTES:
-#
-#      Write your assembly code only in the marked blocks.
-#
-#      DO NOT change anything outside the marked blocks.
-#
-#      Remember to fill in your name, Red ID in the designated fields.
-#
-#
-#      
+      
 ###############################################################
 #                           Data Section
 .data
